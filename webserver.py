9b1e6c2690ecb1c027dcdcffaed3c825a3efe22f
tbonelaforge <ttf0809@gmail.com>
2017-02-16 14:58:53 -0800

    Updating server for arithmetic3.

--- webserver.py
+++ webserver.py
@@ -3,23 +3,19 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return """<!DOCTYPE html>
-<html>
-  <head>
-    <title>Terry Ford's Website</title>
-  </head>
-  <body>
-    <h1>Hello! My name is Terry Ford.</h1>
-    <img src="static/good-picture.jpeg" width="420" title="A picture of me, when this website was a mere twinkle in my eye." alt="Terry at a Baseball Game">
-    <a href="arithmetic"><h2>Arithmetic Game</h2></a>
-  </body>
-</html>"""
+def home_page():
+    html = render_template('homepage/index.html')
+    return html
 
 @app.route('/arithmetic')
 def arithmetic_start_page():
-    html = render_template("arithmetic/game-container.html");
-    return html;
+    html = render_template("arithmetic/game-container.html")
+    return html
+
+@app.route('/arithmetic3')
+def arithmetic2_start_page():
+    html = render_template("arithmetic3/game-container.html")
+    return html
 
 @app.route('/walkthrough')
 def walkthrough():
@@ -27,5 +23,5 @@ def walkthrough():
 
 @app.route('/walkthrough/<walkthrough_step>')
 def walkthrough_page(walkthrough_step="1"):
-    html = render_template("arithmetic/walkthrough.html", step=walkthrough_step);
-    return html;
+    html = render_template("arithmetic/walkthrough.html", step=walkthrough_step)
+    return html
